a85d0aeb52e6a06b6377a979973969f8cb8a4121
Davis Owen <davisowen@Daviss-MacBook-Pro.local>
2021-04-30 09:46:34 -0700

    fixed syntax error

--- trellis/num_to_english/views.py
+++ trellis/num_to_english/views.py
@@ -62,7 +62,7 @@ def num_to_english(request):
     except ValueError:
         return Response({'status': 'fail', 'error': 'incorrect number supplied'})
     if int_num >= 10**12 or int_num < 0:
-        return Response(['status': 'fail', 'error': 'only numbers between 0 and 10^12 supported'])
+        return Response({'status': 'fail', 'error': 'only numbers between 0 and 10^12 supported'})
     result = ''
     if int_num == 0:
         result = 'zero'
